c10d3994878b35799cd1564597fa957dab2b1b34
gabinnemangou <Steve.nemangou@protonmail.com>
2025-05-24 16:12:15 +0200

    new

---
+++ src/test/java/de/club/usermanager/UserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.club.usermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
